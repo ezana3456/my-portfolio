91c1a0fa6beac321da8c8240bc99827cf1896a53
ezana3456 <ezedingulte40@gmail.com>
2025-05-25 03:23:50 -0900

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,20 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   base: '/my-portfolio/',
+    build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    // Optimizations for static site
+    minify: 'terser',
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          icons: ['react-icons']
+        }
+      }
+    }
+  }
+
 })
